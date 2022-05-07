b899aaa3feb567c06b9b9b79623bc1e397f6279c
shu Lin <bbqlinshu@gmail.com>
2022-05-06 22:45:49 -0400

    updated user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
     has_secure_password
-    validates :username, presence: true
+    validates :username, presence: true, uniqueness: true
     validates :age, numericality: {minimum: 18}
     has_many :apartments
     has_many :types, through: :apartments
